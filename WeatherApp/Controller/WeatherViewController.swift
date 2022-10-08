7f7e0d0b5e140b98ddd6b87620d0fe62f5e85aa8
lcy <pgbenny0035@gmail.com>
2022-10-09 00:28:28 +0900

    refector: 새로운 ViewController IBOutlet 연결

--- WeatherApp/Controller/WeatherViewController.swift
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -12,6 +12,47 @@ import Gifu
 class WeatherViewController: UIViewController {
     @IBOutlet weak var WeatherAnimationView: UIImageView!
     
+    @IBOutlet weak var scrollbarTimeLabel01: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel02: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel03: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel04: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel05: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel06: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel07: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel08: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel09: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel10: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel11: UILabel!
+    @IBOutlet weak var scrollbarTimeLabel12: UILabel!
+    
+    @IBOutlet weak var scrollbarTempLabel01: UILabel!
+    @IBOutlet weak var scrollbarTempLabel02: UILabel!
+    @IBOutlet weak var scrollbarTempLabel03: UILabel!
+    @IBOutlet weak var scrollbarTempLabel04: UILabel!
+    @IBOutlet weak var scrollbarTempLabel05: UILabel!
+    @IBOutlet weak var scrollbarTempLabel06: UILabel!
+    @IBOutlet weak var scrollbarTempLabel07: UILabel!
+    @IBOutlet weak var scrollbarTempLabel08: UILabel!
+    @IBOutlet weak var scrollbarTempLabel09: UILabel!
+    @IBOutlet weak var scrollbarTempLabel10: UILabel!
+    @IBOutlet weak var scrollbarTempLabel11: UILabel!
+    @IBOutlet weak var scrollbarTempLabel12: UILabel!
+    
+    @IBOutlet weak var scrollbarImageView01: UIImageView!
+    @IBOutlet weak var scrollbarImageView02: UIImageView!
+    @IBOutlet weak var scrollbarImageView03: UIImageView!
+    @IBOutlet weak var scrollbarImageView04: UIImageView!
+    @IBOutlet weak var scrollbarImageView05: UIImageView!
+    @IBOutlet weak var scrollbarImageView06: UIImageView!
+    @IBOutlet weak var scrollbarImageView07: UIImageView!
+    @IBOutlet weak var scrollbarImageView08: UIImageView!
+    @IBOutlet weak var scrollbarImageView09: UIImageView!
+    @IBOutlet weak var scrollbarImageView10: UIImageView!
+    @IBOutlet weak var scrollbarImageView11: UIImageView!
+    @IBOutlet weak var scrollbarImageView12: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         playMp4Video(name: "DaySnow")
